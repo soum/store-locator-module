26138b76c071bc39933411bbd0b695efee920e5d
soum <sghosh1@pfsweb.com>
2017-04-24 17:14:58 -0400

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 var debug = process.env.NODE_ENV !== "production";
+var path = require("path");
 var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  entry: {
+    app: ["./js/client.js"]
+  },
   output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
+    path: path.resolve(__dirname, "build"),
+    filename: "js/client.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        //test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
