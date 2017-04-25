d2672944a000e04339733c34395985dbe6da8e78
soum <sghosh1@pfsweb.com>
2017-04-25 11:48:55 -0400

    adding readme

--- js/components/header/Title.js
+++ js/components/header/Title.js
@@ -4,7 +4,9 @@ import React from "react";
 export default class Title extends React.Component {
   render() {
     return (
+    <inputdisplay>
       <h1>{this.props.title}</h1>
+    </inputdisplay>
     );
   }
 }
